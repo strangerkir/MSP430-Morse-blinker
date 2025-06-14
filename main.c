a847b72e08cbd704158c749fa75423133e6ed4a4
Kyrylo <strangerkir@gmail.com>
2025-06-14 20:57:15 +0300

    Init

---
+++ main.c
@@ -0,0 +1,87 @@
+/* --COPYRIGHT--,BSD
+ * Copyright (c) 2017, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * --/COPYRIGHT--*/
+
+#include "driverlib.h"
+
+//******************************************************************************
+//!
+//!   Converts text into Morse signals outputted by led.
+//!
+//******************************************************************************
+
+void setUpTimer(unsigned short clockSourceDivider) {
+    Timer_A_initUpModeParam initParam;
+    initParam.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
+    initParam.clockSourceDivider = clockSourceDivider;
+    initParam.timerPeriod = 0x0FFFF;
+    initParam.captureCompareInterruptEnable_CCR0_CCIE = TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE;
+    //initParam.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_ENABLE;
+    initParam.timerClear = TIMER_A_DO_CLEAR;
+    initParam.startTimer = false;
+
+    Timer_A_initUpMode(TIMER_A0_BASE, &initParam);
+
+    Timer_A_clearCaptureCompareInterrupt(TIMER_A0_BASE,
+    TIMER_A_CAPTURECOMPARE_REGISTER_0);
+
+    Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+}
+
+void setUpLong(void) {
+    setUpTimer(TIMER_A_CLOCKSOURCE_DIVIDER_32);
+};
+
+void setUpShort(void) {
+    setUpTimer(TIMER_A_CLOCKSOURCE_DIVIDER_16);
+}
+
+void setUpPause (void) {
+    setUpTimer(TIMER_A_CLOCKSOURCE_DIVIDER_32);
+}
+
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void myISR_TA0_Other(void) {
+    GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
+}
+
+void main (void)
+{
+    char * text = "Test";
+
+    WDT_A_hold(WDT_A_BASE);
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+    _enable_interrupt();
+    setUpLong();
+    while (1) {
+        __no_operation();
+    }
+}
