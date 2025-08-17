d65627fe8013b0e0650f529afe45e3b4e7ca66b6
Kyrylo <strangerkir@gmail.com>
2025-08-17 21:29:31 +0300

    Move char map to a source file from header

---
+++ src/char-signals_map.c
@@ -0,0 +1,55 @@
+/**
+ * Map character - set of Morse signals.
+ */
+
+#include "iterator.h"
+#include "signals.h"
+#include "char-signals_map.h"
+
+struct CharPattern charMap[] = {
+    {'A', {dit, dah, 0}},
+    {'B', {dah, dit, dit, dit, 0}},
+    {'C', {dah, dit, dah, dit, 0}},
+    {'D', {dah, dit, dit, 0}},
+    {'E', {dit, 0}},
+    {'F', {dit, dit, dah, dit, 0}},
+    {'G', {dah, dah, dit, 0}},
+    {'H', {dit, dit, dit, dit, 0}},
+    {'I', {dit, dit, 0}},
+    {'J', {dit, dah, dah, dah, 0}},
+    {'K', {dah, dit, dah, 0}},
+    {'L', {dit, dah, dit, dit, 0}},
+    {'M', {dah, dah, 0}},
+    {'N', {dah, dit, 0}},
+    {'O', {dah, dah, dah, 0}},
+    {'P', {dit, dah, dah, dit, 0}},
+    {'Q', {dah, dah, dit, dah, 0}},
+    {'R', {dit, dah, dit, 0}},
+    {'S', {dit, dit, dit, 0}},
+    {'T', {dah, 0}},
+    {'U', {dit, dit, dah, 0}},
+    {'V', {dit, dit, dit, dah, 0}},
+    {'W', {dit, dah, dah, 0}},
+    {'X', {dah, dit, dit, dah, 0}},
+    {'Y', {dah, dit, dah, dah, 0}},
+    {'Z', {dah, dah, dit, dit, 0}},
+
+    {'1', {dit, dah, dah, dah, dah, 0}},
+    {'2', {dit, dit, dah, dah, dah, 0}},
+    {'3', {dit, dit, dit, dah, dah, 0}},
+    {'4', {dit, dit, dit, dit, dah, 0}},
+    {'5', {dit, dit, dit, dit, dit, 0}},
+    {'6', {dah, dit, dit, dit, dit, 0}},
+    {'7', {dah, dah, dit, dit, dit, 0}},
+    {'8', {dah, dah, dah, dit, dit, 0}},
+    {'9', {dah, dah, dah, dah, dit, 0}},
+    {'0', {dah, dah, dah, dah, dah, 0}},
+
+    {'.', {dit, dah, dit, dah, dit, dah, 0}},
+    {',', {dah, dah, dit, dit, dah, dah, 0}},
+    {'-', {dah, dit, dit, dit, dit, dah, 0}},
+    {'?', {dit, dit, dah, dah, dit, dit, 0}},
+    {'!', {dah, dit, dah, dit, dah, dah, 0}},
+    {'\0', {0}},
+};
+
--- src/iterator.c
+++ src/iterator.c
@@ -3,12 +3,32 @@
  */
 
  #include "iterator.h"
+ #include "signals.h"
+ #include "char-signals_map.h"
 
 struct Caller caller;
+
+void getNextChar(struct Caller *caller) {
+    caller->fp[0] = dit;
+}
+
+struct CharPattern findCharPattern(unsigned char toFind) {
+    int i = 0;
+    do {
+        if(charMap[i].ch == toFind) {
+            return charMap[1];
+        }
+
+        i++;
+    } while(charMap[i].ch != '\0');
+
+    return charMap[--i];
+}
 
 void callNext() {
 
     if(caller.idx >= caller.size) {
+        getNextChar(&caller);
         caller.idx = 0;
     }
 
