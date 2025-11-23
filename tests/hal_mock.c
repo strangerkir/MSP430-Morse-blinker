970338fa06aae2c696ba51876bc9791b5a2d881e
Kyrylo <strangerkir@gmail.com>
2025-11-23 17:08:24 +0200

    Add mocks for hal module

---
+++ tests/hal_mock.c
@@ -0,0 +1,25 @@
+#include "hal.h"
+
+void setIsrCallback(isrCallback) {
+
+}
+
+void ledOn() {
+
+}
+
+void ledOff() {
+
+}
+
+void setUpTimer(unsigned short timerPeriod) {
+    
+}
+
+void init() {
+
+}
+
+void waitUnits(unsigned short units) {
+    
+}
