252b8507b3d1c0eb8bee5377e9bf1e1d937ddca0
Kyrylo <strangerkir@gmail.com>
2026-02-14 15:03:37 +0200

    Store text size + use proper type for it

--- include/char_iterator.h
+++ include/char_iterator.h
@@ -2,10 +2,12 @@
 #define CHAR_ITERATOR_H
 
 #include <stdbool.h>
+#include <stddef.h>
 
 typedef struct {
     char text[1024];
-    int pos;
+    size_t len;
+    size_t pos;
  } CharIterator;
 
 void CharIterator_init(CharIterator *st, char *text);
--- src/char_iterator.c
+++ src/char_iterator.c
@@ -8,16 +8,18 @@
 CharIterator charIterator;
 
 void CharIterator_init(CharIterator *charIterator, char *text){
+    charIterator->len = strlen(text);
     charIterator->pos = 0;
     strcpy(charIterator->text, text);
 }
 
 bool CharIterator_emit(CharIterator *st) {
-    if(st->pos >= strlen(st->text)) {
+    if(st->pos >= st->len) {
         return false; //End of the string
     }
 
     //todo: call SignalsIterator here
 
+    st->pos++;
     return true;
 }
